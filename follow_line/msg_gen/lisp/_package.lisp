92dfc9f991cb9f71df5a373426a55dc2fee4a395
AR-ruediger <ulrich.berger1@gmail.com>
2012-06-19 16:10:27 +0200

    nichts

--- follow_line/msg_gen/lisp/_package.lisp
+++
@@ -1,7 +0,0 @@
-(cl:defpackage follow_line-msg
-  (:use )
-  (:export
-   "<LINEPOS>"
-   "LINEPOS"
-  ))
-
--- follow_line/msg_gen/lisp/_package_LinePos.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package follow_line-msg)
-(cl:export '(X-VAL
-          X
-          Y-VAL
-          Y
-          ANGLE-VAL
-          ANGLE
-))
