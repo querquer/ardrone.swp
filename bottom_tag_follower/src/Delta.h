3053da1deaf86289aee80f9187c9b223ee389f58
AR-ruediger <ulrich.berger1@gmail.com>
2012-06-27 19:15:44 +0200

    umstrukturieren

--- bottom_tag_follower/src/Delta.h
+++ bottom_tag_follower/src/Delta.h
@@ -7,26 +7,8 @@
 class Delta
 {
 public:
-	Delta()
-	{
-		old_value = 0;
-		old_time = 0;
-		old_vel = 0;
-	}
-	float get_velocity(float new_value)
-	{
-		time_t new_time = time(NULL);
-		//Geschwindigkeit = Weg/Zeit
-		float vel = (new_value - old_value) / (new_time - old_time);
-
-		//smooth Velocity
-		old_vel = old_vel + (vel - old_vel) * 0.1;
-
-		old_time = new_time;
-		old_value = new_value;
-
-		return old_value;
-	}
+	Delta();
+	float get_velocity(float new_value);
 private:
 
 	float old_value;
