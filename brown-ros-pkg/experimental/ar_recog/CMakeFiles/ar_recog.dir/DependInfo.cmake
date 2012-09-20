7cf8bcc47fea00f81e75dbdddb72d4999aab059a
AR-ruediger <ulrich.berger1@gmail.com>
2012-09-20 11:32:50 +0200

    recog added

---
+++ brown-ros-pkg/experimental/ar_recog/CMakeFiles/ar_recog.dir/DependInfo.cmake
@@ -0,0 +1,18 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/ulrich/ros_workspace/brown-ros-pkg/experimental/ar_recog/src/ARToolKit/examples/loadMultiple/object.c" "/home/ulrich/ros_workspace/brown-ros-pkg/experimental/ar_recog/CMakeFiles/ar_recog.dir/src/ARToolKit/examples/loadMultiple/object.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ulrich/ros_workspace/brown-ros-pkg/experimental/ar_recog/src/ar_recog.cpp" "/home/ulrich/ros_workspace/brown-ros-pkg/experimental/ar_recog/CMakeFiles/ar_recog.dir/src/ar_recog.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
